313d20b2363ad801b298f9d8b7827f23ea83d045
dennislee928 <pcleegood@gmail.com>
2025-02-22 10:12:00 +0800

    :0

---
+++ bde-bind-next-app/src/pages/hello.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HelloPage = () => {
+  return (
+    <div>
+      <h1>Hello, Welcome to the Page!</h1>
+      <p>This is a simple React page in your Next.js application.</p>
+    </div>
+  );
+};
+
+export default HelloPage;
